00ec124bc22cdf59886234d21f368e3eb6d9ff57
defname <bodo.akdeniz@gmail.com>
2025-11-02 15:33:11 +0100

    add declarations to the debug header file

--- src/str8_debug.h
+++ src/str8_debug.h
@@ -2,8 +2,22 @@
 #define STR8_DEBUG_H
 
 #if defined(DEBUG)
+#include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
+
 #define STATIC
 #define INLINE
+
+
+STATIC INLINE size_t get_field(void *field, uint8_t type);
+STATIC INLINE bool value_in_range(uint8_t type, size_t value);
+STATIC INLINE void set_field(void *field, uint8_t type, size_t value);
+STATIC INLINE void *len_field(str8 str, uint8_t type);
+STATIC INLINE void *size_field(str8 str, uint8_t type);
+STATIC INLINE void *cap_field(str8 str, uint8_t type);
+
+
 #else
 #define STATIC static
 #define INLINE inline
